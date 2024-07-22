63d2b7e3ba1dc9b2a0705321d44b072cb7eb20f2
Anthony Udem <anthony11929@gmail.com>
2024-07-22 11:21:57 +0100

    Add files via upload
    generates and displays a band name from your city's and bet's name.

---
+++ band name generator.py
@@ -0,0 +1,10 @@
+def band_name():
+    """" Generates and displays a band name from your city's and bet's name """""
+
+    print("Welcome to the Lonasctech Band Name Generator.")
+    city_name = input('What\'s the name of city you grew up in? ')
+    pet_name = input("What's your pet's name? ")
+    print("Your band name could be " + city_name + " " + pet_name)
+
+
+band_name()
